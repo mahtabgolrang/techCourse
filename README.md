f385553747973486605d65056f9f773275312f29
mrso26 <mr.soltani200@gmail.com>
2021-12-11 06:29:53 +0330

    change data base from postgresql to sql lite

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # techCourse
 This a university project for software engineering course.
+
+
+admin 
+user = admin
+pass = admin
--- techCours/techCourse/settings.py
+++ techCours/techCourse/settings.py
@@ -79,13 +79,20 @@ WSGI_APPLICATION = 'techCourse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'techCourse',
+#         'USER':'postgres',
+#         'PASSWORD':1234,
+#         'PORT':5432
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'techCourse',
-        'USER':'postgres',
-        'PASSWORD':1234,
-        'PORT':5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
